c5727b663abd5bbb83ff0c36bb3903a53c1426a9
Juanjo Abarca <j.abarcag@outlook.com>
2025-06-11 22:37:13 -0400

    Update App.java

--- authapp/src/main/java/com/equipo/auth/App.java
+++ authapp/src/main/java/com/equipo/auth/App.java
@@ -1,4 +1,5 @@
 package com.equipo.auth;
+import java.util.logging.Logger;
 
 import java.util.Scanner;
 
@@ -7,15 +8,15 @@ public class App {
         AuthService authService = new AuthService();
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Usuario: ");
+        logger.info("Usuario: ");
         String usuario = sc.nextLine();
-        System.out.print("Contraseña: ");
+        logger.info("Contraseña: ");
         String clave = sc.nextLine();
 
         if (authService.autenticar(usuario, clave)) {
-            System.out.println("¡Acceso concedido!");
+            logger.info("¡Acceso concedido!");
         } else {
-            System.out.println("Credenciales incorrectas.");
+            logger.info("Credenciales incorrectas.");
         }
 
         sc.close();
